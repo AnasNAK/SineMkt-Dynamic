cb7dfb9869de26ad93fac1a41c2402db829a4511
AnasNAK <anasc00ding@gmail.com>
2023-02-02 01:46:43 +0100

    modificated on crud function

--- app/controller/Products.php
+++ app/controller/Products.php
@@ -19,36 +19,28 @@ class Products extends Controller
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            var_dump($_FILES);
-            $count = count($_POST['name']);
+            // sanitize post array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            move_uploaded_file($_FILES['image']['tmp_name'], 'images/upload/' . $_FILES['image']['name']);
 
-            for ($i = 0; $i < $count; $i++) {
-                // sanitize post array
-                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-                move_uploaded_file($_FILES['image']['tmp_name'][$i], 'img/upload/' . $_FILES['image']['name'][$i]);
-
-                $data = [
-                    'name' => $_POST['name'][$i],
-                    'stock' => $_POST['stock'][$i],
-                    'price' => $_POST['price'][$i],
-                    'description' => $_POST['description'][$i],
-                    'image' => $_FILES['image']['name'][$i]
-                ];
+            $data = [
+                'name' => $_POST['name'],
+                'price' => $_POST['price'],
+                'description' => $_POST['description'],
+                'image' => $_FILES['image']['name']
+            ];
 
 
-                //make sure no errors
-                if ($this->productModel->addProduct($data)) {
-                    redirect('Products/dashboardAdmin');
-                } else {
-                    //load view with errors
-                    $this->view('product/addProduct', $data);
-                }
+            //make sure no errors
+            if ($this->productModel->addProduct($data)) {
+                redirect('Products/dashboardAdmin');
+            } else {
+                //load view with errors
+                $this->view('product/addProduct', $data);
             }
         } else {
             $data = [
                 'name' => '',
-                'stock' => '',
                 'price' => '',
                 'description' => '',
                 'image' => '',
@@ -63,7 +55,7 @@ class Products extends Controller
             // sanitize post array
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
 
-            move_uploaded_file($_FILES['image']['tmp_name'], 'img/upload/' . $_FILES['image']['name']);
+            move_uploaded_file($_FILES['image']['tmp_name'], 'images/upload/' . $_FILES['image']['name']);
 
             $product = $this->productModel->findproductById($id);
 
@@ -71,12 +63,10 @@ class Products extends Controller
                 $data = [
                     'id' => $product->id,
                     'name' => trim($_POST['name']),
-                    'stock' => trim($_POST['stock']),
                     'price' => trim($_POST['price']),
                     'description' => trim($_POST['description']),
                     'image' => $product->image,
                     'name_err' => '',
-                    'stock_err' => '',
                     'price_err' => '',
                     'description_err' => '',
                     'image_err' => ''
@@ -85,12 +75,10 @@ class Products extends Controller
                 $data = [
                     'id' => $product->id,
                     'name' => trim($_POST['name']),
-                    'stock' => trim($_POST['stock']),
                     'price' => trim($_POST['price']),
                     'description' => trim($_POST['description']),
                     'image' => $_FILES['image']['name'],
                     'name_err' => '',
-                    'stock_err' => '',
                     'price_err' => '',
                     'description_err' => '',
                     'image_err' => ''
@@ -101,9 +89,6 @@ class Products extends Controller
             if (empty($data['name'])) {
                 $data['name_err'] = 'Please enter name';
             }
-            if (empty($data['stock'])) {
-                $data['stock_err'] = 'Please enter stock';
-            }
             if (empty($data['price'])) {
                 $data['price_err'] = 'Please enter price';
             }
@@ -112,7 +97,7 @@ class Products extends Controller
             }
 
             //make sure no errors
-            if (empty($data['name_err'])  && empty($data['stock_err']) && empty($data['price_err']) && empty($data['description_err'])) {
+            if (empty($data['name_err']) && empty($data['price_err']) && empty($data['description_err'])) {
                 if ($this->productModel->edit($data)) {
                     redirect('products/dashboardAdmin');
                 } else {
@@ -128,12 +113,10 @@ class Products extends Controller
         $data = [
             'id' => $product->id,
             'name' => $product->name,
-            'stock' => $product->stock,
             'price' => $product->price,
             'description' => $product->description,
             'image' => $product->image,
             'name_err' => '',
-            'stock_err' => '',
             'price_err' => '',
             'description_err' => '',
             'image_err' => ''
@@ -144,12 +127,12 @@ class Products extends Controller
     public function dashboardAdmin()
     {
         $products = $this->productModel->getProduct();
-         $productsum=$this->productModel->getProductByStock();
+        //  $productsum=$this->productModel->getProductByStock();
         $data = [
             'products' => $products,
-            'productsum' =>$productsum,
+            // 'productsum' =>$productsum,
         ];
-        $this->view('inc/dashboardAdmin', $data);
+        $this->view('pages/dashboard', $data);
     }
     public function delete($id)
     {
@@ -160,5 +143,4 @@ class Products extends Controller
             die('ops');
         }
     }
-
 }
